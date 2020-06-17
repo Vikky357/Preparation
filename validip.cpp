a6a3e376b668d419ef9474abb561e2d6d5645c87
Vigneh I <53791272+Vikky357@users.noreply.github.com>
2020-06-17 11:05:29 +0530

    Create validip.cpp

---
+++ validip.cpp
@@ -0,0 +1,45 @@
+/*
+
+Write a function to check whether an input string is a valid IPv4 address or IPv6 address or neither.
+
+IPv4 addresses are canonically represented in dot-decimal notation, which consists of four decimal numbers, each ranging from 0 to 255, separated by dots ("."), e.g.,172.16.254.1;
+
+Besides, leading zeros in the IPv4 is invalid. For example, the address 172.16.254.01 is invalid.
+
+IPv6 addresses are represented as eight groups of four hexadecimal digits, each group representing 16 bits. The groups are separated by colons (":"). For example, the address 2001:0db8:85a3:0000:0000:8a2e:0370:7334 is a valid one.
+
+
+Example 1:
+Input: "172.16.254.1"
+
+Output: "IPv4"
+
+Explanation: This is a valid IPv4 address, return "IPv4".
+Example 2:
+Input: "2001:0db8:85a3:0:0:8A2E:0370:7334"
+
+Output: "IPv6"
+
+Explanation: This is a valid IPv6 address, return "IPv6".
+Example 3:
+Input: "256.256.256.256"
+
+Output: "Neither"
+
+Explanation: This is neither a IPv4 address nor a IPv6 address.
+
+
+*/
+
+class Solution {
+public:
+    string validIPAddress(string IP) {
+        regex ip4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+        regex ip6("((([0-9a-fA-F]){1,4})\\:){7}(([0-9a-fA-F]){1,4})");
+        if(regex_match(IP,ip4))
+            return "IPv4";
+        if(regex_match(IP,ip6))
+            return "IPv6";
+        return "Neither";
+    }
+};
